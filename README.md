cdb26b705711ec6941051b888627bfe345f18130
VICTOR M OTTATI <victor.ottati@gmail.com>
2023-05-11 17:38:54 +0100

    FEAT: Add movies API

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ## SERVER Node JS + Express
 
+[REPO](https://github.com/VMO2020/city-explorer-server)
+
 ## RUN
 
 `npm run server`
--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(cors());
 // We are getting the port variable from the .env file.
 const PORT = process.env.PORT || 8080;
 const API_KEY = process.env.API_KEY;
+const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
 
 // This is a route. if you turn the server on and go to http://localhost:8080
 app.get('/', (request, response) => {
@@ -42,8 +43,24 @@ app.get('/weather', async (req, res) => {
 	}
 });
 
+app.get('/movies', async (req, res) => {
+	// https://api.themoviedb.org/3/search/movie?api_key=be09a8ef06ffc31f28b16e326ab9c675&query=Paris&language=en&region=Paris
+
+	try {
+		const city = req.query.city || 'London'; // Default to 'London' if city is not provided
+
+		const response = await fetch(
+			`https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${city}&language=en&region=${city}`
+		);
+		const data = await response.json();
+
+		// Return the data as the response
+		res.json(data);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: 'An error occurred' });
+	}
+});
+
 // Run the server
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
-
-// https://api.themoviedb.org/3/search/movie?api_key=be09a8ef06ffc31f28b16e326ab9c675&query=Paris&language=en&region=Paris
-// https://api.themoviedb.org/3/search/movie?api_key=be09a8ef06ffc31f28b16e326ab9c675&query=London&language=en&region=London
