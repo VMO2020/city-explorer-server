a5031c9447adbd97a68c424bd92e57a9d9303de3
VICTOR M OTTATI <victor.ottati@gmail.com>
2023-05-12 15:28:09 +0100

    UPDATE: weather.js

--- library/weather.js
+++ library/weather.js
@@ -8,10 +8,11 @@ const weather = async (req, res) => {
 
 		const URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`;
 
-		const result = await fetch(URL);
-		const data = await result.json();
+		// const result = await fetch(URL);
+		// const data = await result.json();
 
-		res.status(200).json(data);
+		res.status(200).json(URL);
+		// res.status(200).json(data);
 	} catch (error) {
 		console.error(error);
 		res.status(404).json(error);
